aca858df54dfff92e796f150afc0ef8e82a5ea30
ryanpham <ryanphamit@gmail.com>
2025-09-22 19:34:15 +0700

    Refactor Page component to include background image and improve layout structure

--- app/layout.js
+++ app/layout.js
@@ -24,10 +24,10 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
 			<body
-				className={`${josefinSans.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+				className={`${josefinSans.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
 			>
 				<Header />
-				<div className='flex-1 px-8 py-12'>
+				<div className='flex-1 px-8 py-12 '>
 					<main className='max-w-7xl mx-auto'>{children}</main>
 				</div>
 			</body>
