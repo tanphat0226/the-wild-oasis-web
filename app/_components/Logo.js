aa5ee48686427cdde1c0eff050c463f96709267c
ryanpham <ryanphamit@gmail.com>
2025-09-22 19:23:54 +0700

    Refactor Logo component to use Next.js Image component for improved performance

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 function Logo() {
 	return (
 		<Link href='/' className='flex items-center gap-4 z-10'>
-			<img
+			<Image
 				src='/logo.png'
 				height='60'
 				width='60'
