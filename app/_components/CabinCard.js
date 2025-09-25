aa125d9f7455246c707d6fa8cd55912bbb0f3efe
ryanpham <ryanphamit@gmail.com>
2025-09-25 19:57:28 +0700

    Add Cabin details page with image display and information layout

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -12,6 +12,7 @@ function CabinCard({ cabin }) {
 					src={image}
 					alt={`Cabin ${name}`}
 					fill
+					sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
 					className='object-cover flex-1 border-r border-primary-800'
 				/>
 			</div>
