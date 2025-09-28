7dba0dd1855cfcda143d9963da5ff43939c1f99a
ryanpham <ryanphamit@gmail.com>
2025-09-28 20:15:10 +0700

    Add TextExpander component for expandable text functionality; integrate into cabin description

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -1,6 +1,8 @@
-import { CalendarDaysIcon, HomeIcon, UserIcon } from '@heroicons/react/24/solid'
+'use client'
 import SignOutButton from '@/app/_components/SignOutButton'
+import { CalendarDaysIcon, HomeIcon, UserIcon } from '@heroicons/react/24/solid'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const navLinks = [
 	{
@@ -21,13 +23,15 @@ const navLinks = [
 ]
 
 function SideNavigation() {
+	const pathname = usePathname()
 	return (
 		<nav className='border-r border-primary-900'>
 			<ul className='flex flex-col gap-2 h-full text-lg'>
 				{navLinks.map((link) => (
 					<li key={link.name}>
 						<Link
-							className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+							className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200
+								${pathname === link.href ? 'bg-primary-900' : ''}`}
 							href={link.href}
 						>
 							{link.icon}
