463d06b00ca11d834fafd376d1e5c51feefd9623
ryanpham <ryanphamit@gmail.com>
2025-10-02 20:24:45 +0700

    feat: add LoginMessage component and integrate user session handling in Reservation and Navigation components

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,12 +1,15 @@
-import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service'
+import { auth } from '@/app/_lib/auth'
+import { getBookedDatesByCabinId, getSettings } from '@/app/_lib/data-service'
 import DateSelector from './DateSelector'
 import ReservationForm from './ReservationForm'
+import LoginMessage from './LoginMessage'
 
 async function Reservation({ cabin }) {
 	const [settings, bookedDates] = await Promise.all([
 		getSettings(),
 		getBookedDatesByCabinId(cabin.id),
 	])
+	const session = await auth()
 	return (
 		<div className='grid grid-cols-[auto_auto] border border-primary-800 min-h-[100px]'>
 			<DateSelector
@@ -15,7 +18,11 @@ async function Reservation({ cabin }) {
 				cabin={cabin}
 			/>
 
-			<ReservationForm cabin={cabin} />
+			{session?.user ? (
+				<ReservationForm cabin={cabin} user={session.user} />
+			) : (
+				<LoginMessage />
+			)}
 		</div>
 	)
 }
